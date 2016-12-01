76b49f05789bc49f168217e038348c676d89d348
Hashcode <hashcode0f@gmail.com>
2016-12-01 12:50:45 -0800

    adreno-330: update blobs for android-7.0
    Source files:
    https://github.com/TheMuppets/proprietary_vendor_qcom_binaries/tree/cm-14.1/msm8974/graphics
    
    Updating to android-7.0 binaries fixes codec for viewing YouTube
    and Google Play Movies as well as graphical glitches

--- adreno-330/adreno-330-vendor-blobs.mk
+++ adreno-330/adreno-330-vendor-blobs.mk
@@ -21,18 +21,18 @@ PRODUCT_COPY_FILES += \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
-    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/egl/libplayback_adreno.so:system/vendor/lib/egl/libplayback_adreno.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
-    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
-    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
+    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
-    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
+    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
     $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
-    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so
-
+    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
+    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libscale.so:system/vendor/lib/libscale.so \
+    $(ADRENO_330_FOLDER)/proprietary/vendor/lib/libuiblur.so:system/vendor/lib/libuiblur.so
